78c609be0d6c1010f0cd22f3c6ba5019f64024db
Denmaseno <agus@senomas.com>
2015-07-27 15:12:03 +0000

    Maven artifacts for 1.0.0

--- src/main/java/com/senomas/common/persistence/EntityUnwrapper.java
+++
@@ -1,9 +0,0 @@
-package com.senomas.common.persistence;
-
-import javax.persistence.EntityManager;
-
-public interface EntityUnwrapper<T> {
-
-    T unwrap(EntityManager entityManager);
-    
-}
--- src/main/java/com/senomas/common/persistence/EntityWrapper.java
+++
@@ -1,9 +0,0 @@
-package com.senomas.common.persistence;
-
-import javax.persistence.EntityManager;
-
-public interface EntityWrapper<T> {
-    
-    void wrap(EntityManager entityManager, T object);
-    
-}
